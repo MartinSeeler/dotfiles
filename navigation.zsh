330fb8c6b380bc9717f9716fe7f70b9a0dca1af6
Martin Seeler <mseeler@avantgarde-labs.de>
2016-04-09 23:07:48 +0200

    Add simple navigation aliases

---
+++ navigation.zsh
@@ -0,0 +1,14 @@
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Shortcuts
+alias db="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/Development/projects"
+

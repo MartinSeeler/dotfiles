d80128187d6b19e2ef0bbc9a6549ad71a4c9cbcd
Martin Seeler <mseeler@avantgarde-labs.de>
2016-04-12 22:59:04 +0200

    Add more aliases

--- aliases.zsh
+++ aliases.zsh
@@ -19,10 +19,24 @@ alias ll='ls -lh'
 alias la='ls -lAh'
 
 # Network
+alias webip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
+# fasd commands https://github.com/clvv/fasd
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias c='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+
 # Utils
 alias please='sudo'
 alias cls='clear'
+alias bubu="brew update && brew upgrade --all && brew cleanup"
+alias y='tr -d '\''\n'\'' | pbcopy'

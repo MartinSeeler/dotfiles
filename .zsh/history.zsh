7da017af987826ed8126ae6af5e7f2d053240aac
Martin Seeler <developer@chasmo.de>
2019-01-30 09:23:04 +0100

    Add shared zsh history

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -9,9 +9,11 @@ setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_reduce_blanks
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
+setopt share_history
 
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
